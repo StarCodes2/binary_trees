7d148140c5578a33885d7efb2f7061b0cc9509f6
Ezekiel Ogumewu <anu.ezekiel02@gmail.com>
2024-01-03 06:18:38 -0800

    BUG: When a node has a left child the parent pointer didn't get updated with the new parent address

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,6 +18,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		if (node == NULL)
 			return (NULL);
 
+		if (parent->left != NULL)
+			parent->left->parent = node;
 		node->left = parent->left;
 		parent->left = node;
 	}
