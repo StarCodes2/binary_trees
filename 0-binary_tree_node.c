c4c131b5e8deb89e660df53869464a4f8086fd51
Ezekiel Ogumewu <anu.ezekiel02@gmail.com>
2024-01-03 05:13:54 -0800

    DOC: description added for function binary_tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - Creates a new node of type binary_tree_t
+ * @parent: Points to the parent node
+ * @value: Contains the value to be stored in the node
+ *
+ * Return: A pointer to the new node on success or NULL on failure
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
