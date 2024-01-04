fbd3da968d83ea1d1b6117d7cfd1496c7c2f09de
Ezekiel Ogumewu <anu.ezekiel02@gmail.com>
2024-01-04 05:43:15 -0800

    BUG: Not measuring some trees properly.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,6 +9,20 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (tree != NULL)
+		return (binary_tree_h(tree->left) - binary_tree_h(tree->right));
+	return (0);
+}
+
+/**
+ * binary_tree_h - Measures the height of a binary tree
+ * @tree: Points to the root node of the to be measured
+ *
+ * Return: height of tge tree or 0 if tree is NULL
+ */
+
+int binary_tree_h(const binary_tree_t *tree)
+{
 	int l = 0, r = 0;
 
 	if (tree != NULL)
@@ -17,7 +31,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 			l = 1 + binary_tree_balance(tree->left);
 		if (tree->right != NULL)
 			r = 1 + binary_tree_balance(tree->right);
+		return ((l >= r) ? l : r);
 	}
 
-	return (l - r);
+	return (0);
 }
