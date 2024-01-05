f7016729203380932e97606aee938214cb31d11e
Ezekiel Ogumewu <anu.ezekiel02@gmail.com>
2024-01-04 22:44:41 -0800

    BUG: control reaches end of non-void function

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,11 +9,15 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	binary_tree_t *sibling = NULL;
+
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
 	if (node->parent->left == node)
-		return (node->parent->right);
+		sibling = node->parent->right;
 	if (node->parent->right == node)
-		return (node->parent->left);
+		sibling = node->parent->left;
+
+	return (sibling);
 }
